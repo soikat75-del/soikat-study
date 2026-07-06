0a8d0904766358a0bbb6c5628bf301aebdbd3519
Soikat <82596613+soikat75-del@users.noreply.github.com>
2026-07-06 23:11:42 +0600

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,29 @@
+function updateClock(){
 
+    const now = new Date();
+
+    const date = now.toLocaleDateString("en-GB",{
+        weekday:"long",
+        day:"numeric",
+        month:"long",
+        year:"numeric",
+        timeZone:"Asia/Dhaka"
+    });
+
+    const time = now.toLocaleTimeString("en-US",{
+        hour:"2-digit",
+        minute:"2-digit",
+        second:"2-digit",
+        hour12:true,
+        timeZone:"Asia/Dhaka"
+    });
+
+    document.getElementById("todayDate").innerHTML=date;
+
+    document.getElementById("liveTime").innerHTML=time;
+
+}
+
+setInterval(updateClock,1000);
+
+updateClock();
